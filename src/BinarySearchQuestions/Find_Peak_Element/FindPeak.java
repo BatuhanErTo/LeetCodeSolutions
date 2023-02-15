c0e2da9156072e7243bf8635bff5447f5943deb2
Batuhan Erol <bataer57@gmail.com>
2023-02-15 17:54:20 +0300

    Find Peak Element has been solved

---
+++ src/BinarySearchQuestions/Find_Peak_Element/FindPeak.java
@@ -0,0 +1,17 @@
+package BinarySearchQuestions.Find_Peak_Element;
+
+public class FindPeak {
+    public int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]>nums[mid+1]){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
