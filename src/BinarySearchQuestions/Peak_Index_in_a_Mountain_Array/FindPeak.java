447fcf34a14db58b18743d32bc3b27f5cba6ce87
Batuhan Erol <bataer57@gmail.com>
2023-02-15 17:40:05 +0300

    Peak Index in a Mountain Array question has been solved

---
+++ src/BinarySearchQuestions/Peak_Index_in_a_Mountain_Array/FindPeak.java
@@ -0,0 +1,17 @@
+package BinarySearchQuestions.Peak_Index_in_a_Mountain_Array;
+//https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+public class FindPeak {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(arr[mid]>arr[mid+1]){
+                end = mid;
+            }else{
+                start = mid+1;
+            }
+        }
+        return start;
+    }
+}
