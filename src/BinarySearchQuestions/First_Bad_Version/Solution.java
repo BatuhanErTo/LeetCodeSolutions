4f98486f842af2d30d4683f70f423effa8d88067
Batuhan Erol <bataer57@gmail.com>
2023-02-24 23:20:05 +0300

    Valid_Perfect_Square, Two_Sum_II_Input_Array_Is_Sorted, First_Bad_Version solutions added

---
+++ src/BinarySearchQuestions/First_Bad_Version/Solution.java
@@ -0,0 +1,21 @@
+package BinarySearchQuestions.First_Bad_Version;
+//https://leetcode.com/problems/first-bad-version/description/
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+/**
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int lowest = 1;
+        int highest = n;
+        while(lowest<=highest){
+            int mid = lowest + (highest-lowest)/2;
+            if(isBadVersion(mid)){
+                highest = mid - 1;
+            }else{
+                lowest = mid + 1;
+            }
+        }
+        return lowest;
+    }
+}
+ **/
