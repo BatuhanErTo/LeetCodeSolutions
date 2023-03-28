54505ef592b2070446438d269ed24a882502a720
Batuhan Erol <bataer57@gmail.com>
2023-03-28 22:28:50 +0300

    count-primes has been solved, updated

--- src/Count_Primes/Solution.java
+++ src/Count_Primes/Solution.java
@@ -1,24 +1,20 @@
 package Count_Primes;
+
+import java.util.Arrays;
+
 // https://leetcode.com/problems/count-primes/
 class Solution {
     public int countPrimes(int n) {
         boolean[] flags = new boolean[n];
-        for(int i = 2; i < n; i++){
-            flags[i] = true;
-        }
-
+        Arrays.fill(flags, true);
+        int counter = 0;
         for(int i = 2; i < n; i++){
             if(flags[i]){
+                counter++;
                 for(int j = 2*i; j < n; j += i){
                     flags[j] = false;
                 }
             }
-        }
-
-        int counter = 0;
-        for(int i = 2; i < n; i++){
-            if(flags[i])
-                counter++;
         }
         return counter;
     }
